7baadabb396affec5e819457e5dcd8be893e4974
Sadok Anaya <anaya.sadok@gmail.com>
2026-02-05 12:20:56 +0100

    Add encrypt/decrypt endpoints and .NET gitignore

--- CryptoApi/Program.cs
+++ CryptoApi/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -14,28 +13,47 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/encrypt", (string text, int shift) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var encrypted = Caesar(text, shift);
+    return Results.Ok(new { input = text, shift, output = encrypted });
+});
+
+
+app.MapPost("/decrypt", (string text, int shift) =>
+{
+    var decrypted = Caesar(text, -shift);
+    return Results.Ok(new { input = text, shift, output = decrypted });
+});
+
+
+static string Caesar(string input, int shift)
+{
+    if (string.IsNullOrEmpty(input)) return input;
+
+    shift = shift % 26; // håller shift inom alfabetet
+
+    char ShiftChar(char c, char a)
+    {
+        int offset = c - a;
+        int shifted = (offset + shift + 26) % 26;
+        return (char)(a + shifted);
+    }
+
+    var chars = input.ToCharArray();
+
+    for (int i = 0; i < chars.Length; i++)
+    {
+        char c = chars[i];
+
+        if (c >= 'a' && c <= 'z') chars[i] = ShiftChar(c, 'a');
+        else if (c >= 'A' && c <= 'Z') chars[i] = ShiftChar(c, 'A');
+        // annars lämnar vi tecknet som det är
+    }
+
+    return new string(chars);
+}
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
